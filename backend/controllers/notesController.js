569dec0bbc91cf1d86538b8f5b9f3c8f0d8043a4
hervekwizera <hervekwizera63@gmail.com>
2025-06-26 13:37:00 +0200

    creation of controllers folder and notesController

--- backend/controllers/notesController.js
+++ backend/controllers/notesController.js
@@ -1,3 +1,16 @@
 export  function getAllNotes (req,res){
   res.status(200).send("my name is smoking mirror.");
 }
+
+export  function createNote(req,res){
+  res.status(201).json({message:"the note is created successfully"});
+}
+
+export  function updateNote (req,res){
+  res.status(200).json({message:"the note is updated successfully"});;
+}
+
+export  function deleteNote (req,res){
+  res.status(200).json({message:"the note deleted successfully"});
+}
+
--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -1,20 +1,11 @@
 import express, { Router } from "express";
-import { getAllNotes } from "../controllers/notesController.js";
+import { getAllNotes,createNote,updateNote,deleteNote } from "../controllers/notesController.js";
 
 const noteRouter = express.Router();
 
-noteRouter.get("/",getAllNotes)
-
-noteRouter.post("/",(req,res)=>{
-  res.status(201).json({message:"the note is created successfully"});
-})
-
-noteRouter.put("/:id",(req,res)=>{
-  res.status(200).json({message:"the note is updated successfully"});;
-})
-
-noteRouter.delete("/:id",(req,res)=>{
-  res.status(200).json({message:"the note deleted successfully"});
-})
+noteRouter.get("/",getAllNotes);
+noteRouter.post("/",createNote);
+noteRouter.put("/:id",updateNote);
+noteRouter.delete("/:id",deleteNote);
 
 export default noteRouter;
