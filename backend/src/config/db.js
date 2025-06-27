e9930ddff5d2cf8b763cade9a755c3cc62583a36
hervekwizera <hervekwizera63@gmail.com>
2025-06-27 18:25:53 +0200

    modification of database config file

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
-export const connectDB = async ()=>{
-    try {
-        mongoose.connect(process.env.MONGO_URI);
-        console.log('CONNECTED TO MONGOODB SUCCESSFULLY!');
-    } catch (error) {
-        console.error('Error connected to mongoodb:', error);
-        process.exit(1); //export with failure. 
-    }
-}
+export const connectDB = async () => {
+  try {
+     await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB Connected successfully!");
+  } catch (error) {
+    console.error("❌ MongoDB Connection Error:", error.message);
+    process.exit(1); // Exit process with failure
+  }
+};
