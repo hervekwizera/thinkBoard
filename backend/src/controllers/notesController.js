79a186de9e6954da09d695c50f176aa39fad41d8
hervekwizera <hervekwizera63@gmail.com>
2025-06-30 20:23:48 +0200

    creating getNoteById  but still have some issue

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,16 +1,38 @@
 import Note from "../models/Note.js";
-export async function getAllNotes (req,res){
+
+
+export async function getAllNote(req,res) {
   try {
-    const notes = await Note.find()
-    res.status(200).json(notes)
+    const note = await Note.find().sort({createdAt: -1})
+    res.status(200).json(note);
   } catch (error) {
-    console.error("Error in getAllNotes controller", error)
-  res.status(500).json({message:"Internal server error"})
+    console.error("Error in getting all note:", error);
+    res.status(500).json({message:"Internal server error"});
+  }
+  
+}
+
+export async function getNoteById(req,res){
+  try {
+    const note = await Note.findById(req.param.id);
+    if (!note) return res.status(404).json({message:"Note not found"});
+    res.status(200).json(note);
+  } catch (error) {
+    console.error("Error in getNOTE controller :", error);
+    res.status(500).json({message:"Internal server error"});
   }
 }
 
-export  function createNote(req,res){
-  res.status(201).json({message:"the note is created successfully"});
+export async  function createNote(req,res){
+  try {
+    const {title,content} = req.body
+    const newNote = new Note({title,content});
+    const savedNote = await newNote.save();
+    res.status(201).json(savedNote)
+  } catch (error) {
+     console.error("Error in creatNOTE controller :", error);
+    res.status(500).json({message:"Internal server error"});
+  }
 }
 
 export  function updateNote (req,res){
--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -1,9 +1,10 @@
 import express, { Router } from "express";
-import { getAllNotes,createNote,updateNote,deleteNote } from "../controllers/notesController.js";
+import { getAllNotes,createNote,updateNote,deleteNote,getNoteById } from "../controllers/notesController.js";
 
 const noteRouter = express.Router();
 
 noteRouter.get("/",getAllNotes);
+noteRouter.get("/:id",getNoteById);
 noteRouter.post("/",createNote);
 noteRouter.put("/:id",updateNote);
 noteRouter.delete("/:id",deleteNote);
