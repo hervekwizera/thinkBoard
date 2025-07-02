4eafdce45acfed39d03af840a743b622d9f05dfc
hervekwizera <hervekwizera63@gmail.com>
2025-07-02 11:00:20 +0200

    installing upstash packages

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -12,7 +12,7 @@ export async function getAllNotes(req,res){
 
 export async function getNoteById(req,res){
   try {
-    const note = await Note.findById(req.param.id);
+    const note = await Note.findById(req.params.id);
     if(!note) return res.status(404).json({message:"Note not found"});
     res.status(200).json(note);
   } catch (error) {
@@ -35,11 +35,31 @@ export async function createNote(req,res){
 }
 
 
-export  function updateNote (req,res){
-  res.status(200).json({message:"the note is updated successfully"});;
+export async  function updateNote (req,res){
+  try {
+    const {title,content}= req.body;
+    const updatedNote = await Note.findByIdAndUpdate(
+      req.params.id,
+      {title,content},{
+        new: true,
+      })
+    if(!updateNote) return res.status(404).json({message:"Note not found"});
+
+    res.status(200).json(updateNote)
+  } catch (error) {
+    console.error("Error in updateNote controller:",error);
+    res.status(500).json({message:"Internal server error"});
+  }
 }
 
-export  function deleteNote (req,res){
-  res.status(200).json({message:"the note deleted successfully"});
+export async function deleteNote (req,res){
+  try {
+    const deletedNote = await Note.findByIdAndDelete(req.params.id);
+    if(!deletedNote) return res.status(404).json({message:"Note not found"});
+    res.status(200).json(deleteNote)
+  } catch (error) {
+    console.error("Error in deleteNote controller:",error);
+    res.status(500).json({message:"Internal server error"});
+  }
 }
 
