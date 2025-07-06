13eae7e48048b42c91119ec17c1ce2bacf289e01
hervekwizera <hervekwizera63@gmail.com>
2025-07-06 13:40:58 +0200

    installation and tasting toast package

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import {Routes,Route} from "react-router";
+import CreatePage from "./pages/CreatePage.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import NoteDetailPage  from "./pages/NoteDetailPage.jsx";
+import toast from 'react-hot-toast'
+
 
 function App() {
   return (
-    <div>App</div>
+   <div>
+   <button onClick={()=>toast.error("Duel")}>click me</button>
+    <Routes>   
+      <Route path = "/"   element = {<HomePage/>} />
+      <Route path = "/create"   element = {<CreatePage/>} />
+      <Route path = "/note/:id"   element = {<NoteDetailPage/>} />
+    </Routes>
+   </div>
   )
 }
 
---
+++ frontend/src/pages/CreatePage.jsx
@@ -0,0 +1,9 @@
+
+
+function CreatePage() {
+  return (
+    <div>CreatePage</div>
+  )
+}
+
+export default CreatePage
