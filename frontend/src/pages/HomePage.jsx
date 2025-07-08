9ad02223ad0c22e32246a46665d6c73e3ce55c9f
hervekwizera <hervekwizera63@gmail.com>
2025-07-08 11:13:46 +0200

    trying to build home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import {useState} from 'react';
+
+import Navbar from "../components/Navbar.jsx";
+import RateLimitedUI from '../components/RateLimitedUI.jsx';
+import { useEffect } from 'react';
+import axios from "axios";
 
 function HomePage() {
-  return (
-    <div>HomePage</div>
-  )
+  const [isRateLimited, setIsRateLimited] =useState(false);
+  const [notes,setNotes] = useState([])
+  const [loading,setLoading] = useState(true)
+
+  useEffect(() =>{
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get("http://localhost:5000/api/notes"); 
+        console.log(res.data);
+      } catch (error) {
+        console.log("Error fetching notes");
+      }
+    }
+
+  },[])
+   
+  return <div className="min-h-screen">
+    <Navbar/>
+
+    {isRateLimited && <RateLimitedUI />}
+  </div>
+    
 }
 
 export default HomePage
