c0dce97d2dbfb8190a52f4664ef15c1818ed4935
hervekwizera <hervekwizera63@gmail.com>
2025-07-09 12:23:05 +0200

    re-arrange the code for bug fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,12 +10,12 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT||5000;
-
-app.use(express.json());
-app.use(rateLimiter)
 app.use(cors({
   origin:"http://localhost:5173"
-}))
+}));
+
+app.use(express.json());
+app.use(rateLimiter);
 
 app.use("/api/notes",noteRouter);
 
