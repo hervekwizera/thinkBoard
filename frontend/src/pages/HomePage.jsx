79951d06291db83e5a3660ddd7f7a6c83530b249
hervekwizera <hervekwizera63@gmail.com>
2025-07-09 17:28:13 +0200

    adding setNotes(res.data) to my home page to fetch the data from our database

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -15,7 +15,7 @@ function HomePage() {
     const fetchNotes = async () => {
       try {
         const res = await axios.get("http://localhost:5000/api/notes"); 
-        console.log(res.data);
+        setNotes(res.data);
         setIsRateLimited(false);
       } catch (error) {
         console.log("Error fetching notes");
@@ -38,6 +38,18 @@ function HomePage() {
     {isRateLimited && <RateLimitedUI />}
     <div className="max-w-7xl mx-auto p-4 mt-6">
      {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
+     
+     {notes.length>0 && !isRateLimited && (
+     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+      {notes.map(note =>(
+        <div>
+          {note.title} | {note.content}
+        </div>
+      ))}
+
+     </div>
+
+     )}
     </div>
   </div>
     
