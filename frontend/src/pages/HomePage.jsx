48bc98b5106e5bb96dd22fa8b246178361827c6d
hervekwizera <hervekwizera63@gmail.com>
2025-07-09 16:31:35 +0200

    adding loding component when loading condution meet

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,19 +4,29 @@ import Navbar from "../components/Navbar.jsx";
 import RateLimitedUI from '../components/RateLimitedUI.jsx';
 import { useEffect } from 'react';
 import axios from "axios";
+import toast from "react-hot-toast"
 
 function HomePage() {
   const [isRateLimited, setIsRateLimited] =useState(false);
   const [notes,setNotes] = useState([])
   const [loading,setLoading] = useState(true)
-
+ 
   useEffect(() =>{
     const fetchNotes = async () => {
       try {
         const res = await axios.get("http://localhost:5000/api/notes"); 
         console.log(res.data);
+        setIsRateLimited(false);
       } catch (error) {
         console.log("Error fetching notes");
+        console.log(error);
+        if(error.response?.status === 429){
+          setIsRateLimited(true);
+        }else{
+          toast.error("Failed to load notes")
+        }
+      }finally{  
+        setLoading(false)
       }
     }
    fetchNotes();
@@ -26,6 +36,9 @@ function HomePage() {
     <Navbar/>
 
     {isRateLimited && <RateLimitedUI />}
+    <div className="max-w-7xl mx-auto p-4 mt-6">
+     {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
+    </div>
   </div>
     
 }
